96472d01c8b2455435e0203b56f3cb0946201492
Ben Scott <ben@belfryimages.com.au>
2012-01-04 15:10:21 +1000

    message state saving, at least I think?

--- NoteToMe/MainPage.xaml.cs
+++ NoteToMe/MainPage.xaml.cs
@@ -18,23 +18,39 @@ namespace NoteToMe
     public partial class MainPage : PhoneApplicationPage
     {
         const string EMAIL_ADDRESS_KEY = "EmailAddress";
+        const string MESSAGE_KEY = "Message";
 
         IsolatedStorageSettings _isolatedStorageSettings;
-
-
+        bool _isNew;
+        
         public MainPage()
         {
             InitializeComponent();
 
+            _isNew = true;
             _isolatedStorageSettings = IsolatedStorageSettings.ApplicationSettings;
+
             if (_isolatedStorageSettings.Contains(EMAIL_ADDRESS_KEY)) Address.Text = _isolatedStorageSettings[EMAIL_ADDRESS_KEY].ToString();
         }
 
         protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
         {
             _isolatedStorageSettings[EMAIL_ADDRESS_KEY] = Address.Text;
+            this.State[MESSAGE_KEY] = this.Message.Text;
 
             base.OnNavigatedFrom(e);
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (_isNew)
+            {
+                if (this.State.ContainsKey(MESSAGE_KEY)) this.Message.Text = this.State[MESSAGE_KEY].ToString();
+            }
+
+            _isNew = false;
+
+            base.OnNavigatedTo(e);
         }
 
         private void Send_Click(object sender, System.Windows.RoutedEventArgs e)
